1e000a685c5242740c2487e7f5d27df9bf24ae29
Norman <normanvonizara@gmail.com>
2024-11-12 10:24:59 +0300

    Mis en place de l'utilisateur avec Clerk

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import {UserButton, useUser} from "@clerk/nextjs"
+import Link from "next/link"
+
+export default function Navbar () {
+    const {isLoaded, isSignedIn, user} = useUser()
+    return(
+        <div className="bg-base-200/30 px-5 md:px-[10%] py-4">
+            {isLoaded && (
+                (isSignedIn ? (
+                    <>
+                            <div className="flex items-center justify-between">
+                                <div className="flex text-2xl items-center font-bold">
+                                    e<span className="text-accent">.Track</span>
+                                </div>
+                                <div className="md:flex hidden">
+                                    <Link href="" className="btn">Mes Budgets</Link>
+                                    <Link href="" className="btn mx-4">Tableau de bord</Link>
+                                    <Link href="" className="btn">Mes Transactions</Link>
+                                </div>
+                                <UserButton/>
+                            </div>
+                            <div className="md:hidden flex mt-4 justify-center">
+                                <Link href="" className="btn btn-sm">Mes Budgets</Link>
+                                <Link href="" className="btn btn-sm mx-4">Tableau de bord</Link>
+                                <Link href="" className="btn btn-sm">Mes Transactions</Link>
+                            </div>
+                    </>
+                ) : (
+                    <div className="flex items-center justify-between">
+                        <div className="flex text-2xl items-center font-bold">
+                            e<span className="text-accent">.Track</span>
+                        </div>
+                        <div className="flex justify-center">
+                            <Link href="/sign-in" className="btn btn-sm">Se connecter</Link>
+                            <Link href="/sign-up" className="btn btn-sm btn-accent mx-4">S'inscrire</Link>
+                        </div>
+                    </div>
+                ))
+            )}
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import Link from "next/link";
+import Navbar from "@/app/components/Navbar";
 
 export default function Home() {
-  return (
-    <div>
-      <button className="btn btn-accent">test</button>
-    </div>
-  );
+    return (
+        <>
+            <Navbar/>
+            <div>
+                <div className="flex items-center justify-center flex-col py-10 w-full">
+                    <div>
+                        <div className="flex flex-col">
+                            <h1 className="text-4xl md:text-5xl font-bold text-center">
+                                Prenez le contrôle <br/> de vos finances
+                            </h1>
+                            <p className="py-6 text-gray-800 text-center">
+                                Suivez vos budgets et vos dépenses <br/> en toute simplicité avec notre application
+                                intuitive !
+                            </p>
+                            <div className="flex justify-center items-center">
+                                <Link href="/sign-in" className="btn btn-sm md:btn-md btn-outline btn-accent">Se
+                                    connecter</Link>
+                                <Link href="/sign-up" className="btn btn-sm md:btn-md btn-accent ml-2">S'inscrire</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
